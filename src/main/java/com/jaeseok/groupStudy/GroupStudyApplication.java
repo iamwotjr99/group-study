5a99e1df71ea645f78811363590dcdc129ed8c5c
iamwotjr99 <iamwotjr@gmail.com>
2025-07-08 15:57:30 +0900

    init: first commit

---
+++ src/main/java/com/jaeseok/groupStudy/GroupStudyApplication.java
@@ -0,0 +1,13 @@
+package com.jaeseok.groupStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaeseok/groupStudy/GroupStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaeseok.groupStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
