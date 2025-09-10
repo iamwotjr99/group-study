358d9c259314e8bfd28b72916712f14d0ef9ebd6
iamwotjr99 <iamwotjr@gmail.com>
2025-09-10 16:24:32 +0900

    fix(auth): WebMvcTest에서 EnableAuditingJPA 어노테이션 에러를 설정 분리를 통해 해결

---
+++ src/main/java/com/jaeseok/groupStudy/config/JpaAuditingConfig.java
@@ -0,0 +1,10 @@
+package com.jaeseok.groupStudy.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfig {
+
+}
